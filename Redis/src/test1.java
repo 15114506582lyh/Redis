7534422a29cdbb88b98c4a9c925b004d25c83e5d
LYH <1584689584@qq.com>
2022-08-03 11:45:09 +0800

    scdev

--- Redis/src/test1.java
+++ Redis/src/test1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.testng.annotations.Test;
 
 public class test1 {
@@ -14,22 +13,8 @@ public class test1 {
     public void test3(){
         System.out.println("222");
     }
-}
-=======
-import org.testng.annotations.Test;
-
-public class test1 {
     @Test
-    public void test1(){
-        System.out.println("111");
-    }
-    @Test
-    public void test2(){
-        System.out.println("222");
-    }
-    @Test
-    public void test3(){
-        System.out.println("333");
+    public void scdev(){
+        System.out.println("scdev");
     }
 }
->>>>>>> 1dfbd61dd9dc214e3cd4e5e1191e05c975af6bac
